ad21865d5c15e8949bd5feae60cd03d1a62d8deb
Brian <briandsmith1@gmail.com>
2018-02-10 21:18:16 -0500

    Updating function comments.

--- shared/neuralNetworks.py
+++ shared/neuralNetworks.py
@@ -19,6 +19,18 @@ def sigmoidGradient(z):
     return g * (1 - g)
 
 def randInitWeightFlat(nnArch):
+    """ Randomly initializes input weights and returns a flat array
+    
+        The random value for each weight is calculated as (rand * 2 * epsilon - epsilon)
+        where epsilon is calculated at each layer as sqrt(6) / sqrt(nnArch[l+1] + nnArch[l])
+        
+        Args:
+            param1 (List): A List of integers giving the number of units for each layer,
+                            including input & output layers with any number of hidden
+        
+        Returns:
+            array: A vector of all weights in the entire architecture
+    """
     layers = len(nnArch)
     
     for l in range(layers-1):
@@ -30,7 +42,52 @@ def randInitWeightFlat(nnArch):
         
     return thetaArr
 
+def rollupArrayList(flatArray, arch):
+    """ Pass in a flat input weight arrays and get a list of their matrices
+    
+        Args:
+            param1 (array): A flat, concatenated array that contains all weights
+            param2 (List):  A List of integers giving the number of units for each layer,
+                            including input & output layers with any number of hidden
+                            
+        Returns:
+            List: A list of matrices for each layer. Length will be len(param2) - 1
+    """
+    thetaList = []
+    startIndex = 0
+    for i in range(len(arch)-1):
+        endIndex = startIndex + (arch[i] + 1) * arch[i+1]
+        thetaList.append(np.reshape(flatArray[startIndex:endIndex], ((arch[i+1], arch[i] + 1))))
+        startIndex = endIndex
+        
+    return thetaList
+
+def flattenArrayList(arrayList):
+    """ Pass in a list of matrices and get a single flat array
+    
+        Args:
+            param1 (List):  A List of matrices for each layer
+                            
+        Returns:
+            array: Flattened, concatenated array of all weights
+    """
+    flattenedArray = arrayList[0].flatten()
+    for i in range(1, len(arrayList), 1):
+        flattenedArray = np.concatenate((flattenedArray, arrayList[i].flatten()))
+        
+    return flattenedArray
+
 def forwardPropogate(thetaArr, featureIn):
+    """ Go through a single forward pass
+    
+        Args:
+            param1 (List):  A List of weight matrices for each layer
+            param2 (array):  The input vector
+                            
+        Returns:
+            List: List of matrices containing activation values for each node in each layer
+            List: List of matrices containing the input values for each node in each layer
+    """
     layers = len(thetaArr)
     
     a = []
@@ -62,6 +119,17 @@ def cost(thetaArr, X, y):
     return float((1/m)*J)
 
 def costReg(thetaArr, X, y, learningRate):
+    """ Cost function with regularization
+    
+        Args:
+            param1 (List):  A List of weight matrices for each layer
+            param2 (array):  The input vector
+            param3 (array): The expected output values
+            param4 (float): Learning rate
+                            
+        Returns:
+            float: Cost using squared error
+    """
     m = y.shape[0]
     if m == 0:
         return np.inf
@@ -81,6 +149,17 @@ def costReg(thetaArr, X, y, learningRate):
     return float(np.sum(J)/m + regFactor)
 
 def backPropogate(theta, X, oneHotY, learningRate):
+    """ Go through a single backward pass using regularized cost function
+    
+        Args:
+            param1 (List):  A List of weight matrices for each layer
+            param2 (array):  The input vector
+            param3 (array): The expected output values (one hot encoded if necessary)
+            param4 (float): Learning rate
+                            
+        Returns:
+            List: List of matrices containing cost at each node in each layer
+    """
     m = oneHotY.shape[0]
     if m == 0:
         return np.inf
@@ -103,25 +182,22 @@ def backPropogate(theta, X, oneHotY, learningRate):
         bigDelta.append((smallDelta[i].T.dot(a[i]) + learningRate * reg) / m)
     
     return bigDelta
-
-def rollupArrayList(flatArray, arch):
-    thetaList = []
-    startIndex = 0
-    for i in range(len(arch)-1):
-        endIndex = startIndex + (arch[i] + 1) * arch[i+1]
-        thetaList.append(np.reshape(flatArray[startIndex:endIndex], ((arch[i+1], arch[i] + 1))))
-        startIndex = endIndex
-        
-    return thetaList
-
-def flattenArrayList(arrayList):
-    flattenedArray = arrayList[0].flatten()
-    for i in range(1, len(arrayList), 1):
-        flattenedArray = np.concatenate((flattenedArray, arrayList[i].flatten()))
-        
-    return flattenedArray
         
 def nnTop(thetaFlat, X, y, learningRate, nnArch):
+    """ Top level function to be passed into minimizing functions
+    
+        Args:
+            param1 (array):  A flattened array of each weight for each node excluding bias nodes
+            param2 (array):  The input vector
+            param3 (array): The expected output values (one hot encoded if necessary)
+            param4 (float): Learning rate
+            param5 (List):  A List of integers giving the number of units for each layer,
+                            including input & output layers with any number of hidden
+                            
+        Returns:
+            float: Cost using squared error
+            array: A flattened array of the gradient for each node in each layer
+    """
     
     thetaList = rollupArrayList(thetaFlat, nnArch)
     
